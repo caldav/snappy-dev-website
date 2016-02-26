546471ff3636213e134a62bf6d923908e030be24
Didier Roche <didrocks@ubuntu.com>
2016-02-26 13:09:54 +0100

    Adds some comments and cleanify

--- sitegenerator/releases.py
+++ sitegenerator/releases.py
@@ -18,6 +18,8 @@
 # this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
+'''Release-related misc utilities'''
+
 import logging
 import os
 import sys
