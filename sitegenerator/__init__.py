a429b1f33d5fbd719edc8baf6f9674f30e0e27e7
Didier Roche <didrocks@ubuntu.com>
2016-02-29 12:42:22 +0100

    Hook up first function calls for treating the inputs
    We handle right now importing from git, symlinks and import copies

--- sitegenerator/__init__.py
+++ sitegenerator/__init__.py
@@ -21,9 +21,12 @@
 import logging
 import os
 import sys
+import tempfile
 
 from . import settings
-from .releases import get_releases_in_context
+from .fileshandling import import_and_copy_file, replace_variables, reformat_links
+from .gitimporter import import_git_external_branches
+from .releases import get_releases_in_context, load_device_metadata
 
 logger = logging.getLogger(__name__)
 
@@ -44,15 +47,27 @@ def main():
 
     # Loop and switch for each release context
     for release in get_releases_in_context():
-        print(release)
+        output_for_release_dir = os.path.join(settings.OUTPUT_DIR, release)
 
-        # 1. Handle directory copy + setup/ generation for each supported device
-        # we break symlinks for real files
-        # those can use ##IMPORT as well and change content
-        # TODO: check value file!
+        # Load device metadata and variable substitution
+        devices = load_device_metadata()
+
+        # 1. Handle external branch imports (because other pages can import them)
+        with tempfile.TemporaryDirectory() as tmp_dirname:
+            success = import_git_external_branches(output_for_release_dir, tmp_dirname)
+        if not success:
+            sys.exit(1)
+
+        # 2. Handle local directory and files copy + import statements in files
         for path, dirs, files in os.walk(settings.SITE_SRC):
             for file_name in files:
-                print(file_name)
+                os.makedirs(path, exist_ok=True)
+                file_path = os.path.join(path, file_name)
+                dest_path = file_path.replace(settings.SITE_SRC, output_for_release_dir)
+                if not import_and_copy_file(file_path, dest_path):
+                    success = False
+
+
 
         # 2. Handle get-started and prepend "this is part of the tour" link
 
